2f3c8e4a61c77fcdda1c3f11006d5ff60da762e3
Mahesh Babu <maheshbvv@hotmail.com>
2025-07-03 13:36:16 -0400

    Navigation

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,54 @@
+import 'package:donor/home/home.dart';
+import 'package:donor/profile/donor_profile.dart';
+import 'package:donor/search/search.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(const MainApp());
 }
 
-class MainApp extends StatelessWidget {
+class MainApp extends StatefulWidget {
   const MainApp({super.key});
 
   @override
+  State<MainApp> createState() => _MainAppState();
+}
+
+class _MainAppState extends State<MainApp> {
+  int selectedIndex = 0;
+  void onItemTapped(int index) {
+    setState(() {
+      selectedIndex = index;
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+        bottomNavigationBar: BottomNavigationBar(
+          items: const [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.water_drop),
+              label: 'Home',
+            ),
+            BottomNavigationBarItem(icon: Icon(Icons.search), label: 'Search'),
+            BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Profile'),
+          ],
+          currentIndex: selectedIndex,
+          onTap: onItemTapped,
+          selectedItemColor: Colors.red,
+          unselectedItemColor: Colors.grey,
+        ),
+        body: IndexedStack(
+          index: selectedIndex, // Change this index to switch between pages
+          children: [
+            // Replace these with your actual pages
+            PublicHome(),
+            SearchPage(),
+            DonorProfile(),
+          ],
         ),
       ),
     );
---
+++ lib/profile/donor_profile.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class DonorProfile extends StatelessWidget {
+  const DonorProfile({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: Center(child: Text('Donor Profile')));
+  }
+}
